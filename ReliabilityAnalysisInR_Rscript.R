dfbc4daab16b8d1c4d803600eadb177138717c19
CodeOwl94 <CodeOwl94@users.noreply.github.com>
2016-07-04 10:04:01 +0800

    Second version of this file.

--- ReliabilityAnalysisInR_Rscript.R
+++ ReliabilityAnalysisInR_Rscript.R
@@ -9,13 +9,19 @@
 # anything to the right of a hash is a comment / commented-out code.
 
 # setwd("path to where these files are stored")
-
-library(abrem)
 library(SPREDA)
 library(boot)
-abrem.defaults <- options.abrem()
-source("ReliabilitySupportFns.R")
-exa1 <- read.csv("EXA1.csv",header=T)
+library(lattice)
+source("https://raw.githubusercontent.com/CodeOwl94/ross-reliability/master/ReliabilitySupportFns.R")
+exa1 <- read.csv("https://raw.githubusercontent.com/CodeOwl94/ross-reliability/master/EXA1.csv",
+                 header=T)
+####
+
+# Or, load your own dataset here using:
+# exa1 <- read.csv("filename.csv",header=T) 
+# where filename.csv resides within the directory you've set using setwd().
+
+####
 dim(exa1)
 head(exa1) # returns the first 6 rows:
 stack(table(exa1$fail))
@@ -37,18 +43,18 @@ Plot.Observations(exa1.dat[order(exa1.dat$time),][61:nrow(exa1.dat),],
 title("Figure 3", adj=1)
 sort(rank(exa1.dat$time,ties.method="average")[exa1.dat$event==0])
 plot(ecdf(exa1.dat[exa1.dat$event==1,"time"]),main="",xlab="time",
-     verticals=T) 
+     verticals=T,las=1,adj=0.5) 
 abline(v=quantile(exa1.dat[exa1.dat$event==1,"time"],probs=c(0.25,0.5,0.75)),
        col='red',lwd=2,lty=2) # add on quartiles
 title("Figure 4", adj=1)
 hist(exa1.dat[exa1.dat$event==1,"time"],main="",col="lightgrey",
-     xlab="time (non-censored measurements)")
+     xlab="time (non-censored measurements)",las=1,adj=0.5)
 title("Figure 5", adj=1)
 exa1.hist <- hist(exa1.dat$time,plot=F)
-attach(exa1.hist)
+attach(exa1.hist) 
 data.frame(time=mids,n=counts)
-detach(exa1.hist)
-rm(exa1.hist)
+detach(exa1.hist) 
+rm(exa1.hist) 
 Probability.Plots(exa1.dat)
 # title("Figure 6", adj=1)
 Probability.Plots(exa1.dat,dist="Weibull")
@@ -68,11 +74,6 @@ eta.95cl_lo <- exp(summary(exa1.spreda)$coefmat["(Intercept)","95% Lower"])
 eta.95cl_hi <- exp(summary(exa1.spreda)$coefmat["(Intercept)","95% Upper"])
 (beta.ests <- c(lower=beta.95cl_lo,est=beta.spreda,upper=beta.95cl_hi))
 (eta.ests <- c(lower=eta.95cl_lo,est=eta.spreda,upper=eta.95cl_hi))
-exa1.abrem <- Abrem(exa1.dat)
-exa1.abremfit <- abrem.fit(exa1.abrem,
-                           dist = "weibull2p",
-                           method.fit = "mle")
-(exa1.abrem.coef <- Get.coef.W2p(exa1.abremfit))
 Probability.Plots(exa1.dat,dist="Weibull")
 # Get transformed time (x axis) and Fhat (y axis) values: 
 Weibull.xval <- log(Calculate.Fhat(exa1.dat)$time)
@@ -86,25 +87,24 @@ lines(x = Weibull.slope*Weibull.yval + Weibull.intercept,
       y = Weibull.yval,
       col='red', lwd=2)
 title("Figure 8", adj=1)
-exa1.abremfit <- abrem.conf(exa1.abremfit,which="all",
-                            method.conf.blives="lrb",
-                            cl=0.95,
-                            lty=2)
-contour(exa1.abremfit,col.main="white")
-title("Figure 9", adj=1)
+Weibull.Confidence.Region(data=exa1.dat,
+                          model=exa1.spreda,
+                          probability=0.95,
+                          show.contour.labels="FALSE",
+                          title="Figure 9")
 
 # 4. Inference.
 
-hazard.plot.w2p(exa1.abremfit,line.colour="blue")
+hazard.plot.w2p(beta=beta.spreda,eta=eta.spreda,time=exa1.dat$time,line.colour="blue")
 title("Figure 10", adj=1)
-Reliability.plot.w2p(exa1.abremfit,line.colour="blue")
+Reliability.plot.w2p(beta=beta.spreda,eta=eta.spreda,time=exa1.dat$time,line.colour="blue")
 # Then add a vertical line at t=30 to this plot:
 abline(v=30,col="lightgray",lty=2)
 title("Figure 11", adj=1)
-Calc.Unreliability.w2p(exa1.abremfit,time=30)
-(Rel.30 <- round(1 - Calc.Unreliability.w2p(exa1.abremfit,time=30),3))
-Calc.Warranty.w2p(exa1.abremfit,R=Rel.30)
-MTTF.exa1 <- Weibull.2p.Expectation(eta=eta.spreda,beta=beta.spreda)
+Calc.Unreliability.w2p(beta=beta.spreda,eta=eta.spreda,time=30)
+(Rel.30 <- round(1 - Calc.Unreliability.w2p(beta=beta.spreda,eta=eta.spreda,time=30),3))
+Calc.Warranty.w2p(beta=beta.spreda,eta=eta.spreda,R=Rel.30)
+(MTTF.exa1 <- Weibull.2p.Expectation(eta=eta.spreda,beta=beta.spreda))
 ################################################
 # Bootstrapping section: this section takes about 10-15 mins to run.
 set.seed(123)
@@ -127,17 +127,18 @@ mle.pdf <- dweibull(xfit,shape=beta.spreda,
                     scale=eta.spreda)
 plot(xfit,mle.pdf,type='l',col='red',
      xlab="Time To Failure",ylab="f(t)",
-     ylim=c(0,1.25*max(mle.pdf)),bty='l')
+     ylim=c(0,1.25*max(mle.pdf)),bty='l',
+     las=1,adj=0.5,cex.lab=1.2,cex.axis=0.85)
 
 lines(rep(MTTF.exa1,2),c(0,dweibull(MTTF.exa1,
                                     shape=beta.spreda,scale=eta.spreda)),
-                                    lwd=2,col='red')
+      lwd=2,col='red')
 lines(rep(MTTF.95cl_lo,2),c(0,dweibull(MTTF.95cl_lo,
                                        shape=beta.spreda,scale=eta.spreda)),lty=2,
-                                       lwd=2,col='red')
+      lwd=2,col='red')
 lines(rep(MTTF.95cl_hi,2),c(0,dweibull(MTTF.95cl_hi,
                                        shape=beta.spreda,scale=eta.spreda)),lty=2,
-                                       lwd=2,col='red')
+      lwd=2,col='red')
 title("Figure 12", adj=1)
 
 ###
